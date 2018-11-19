2fbf195f1189fd009293ae1ecf37a4e876f0ffd7
zhenjie1 <1304119519@qq.com>
2018-11-19 11:23:52 +0800

    修改路径

--- config/index.js
+++ config/index.js
@@ -44,7 +44,7 @@ module.exports = {
     // Paths
     assetsRoot: path.resolve(__dirname, '../dist'),
     assetsSubDirectory: 'static',
-    assetsPublicPath: '/',
+    assetsPublicPath: '/dist/',
     //assetsPublicPath: './',//app运行环境
 
     /**
