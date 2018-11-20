bbebdee77d6d127ecf44e52ef4bcdfbbf0b5d4f5
zhenjie1 <1304119519@qq.com>
2018-11-20 17:03:50 +0800

    优化

---
+++ build/webpack.dll.config.js
@@ -0,0 +1,20 @@
+const path = require('path')
+const webpack = require('webpack')
+module.exports = {
+	entry: {
+		vendor: ['vue', 'vue-router', 'vuex']
+	},
+	output: {
+		path: path.join(__dirname, '../static'),
+		filename: 'dll.[name].js',
+		library: '[name]'
+	},
+	plugins: [
+		new webpack.DllPlugin({
+			path: path.join(__dirname, '../', '[name]-manifest.json'),
+			name: '[name]'
+		})
+	]
+}
+//主分支
+//分支
---
+++ dist/static/js/17.615b06b2393fe7cf11b3.js
@@ -0,0 +1,1 @@
+webpackJsonp([17],{JJTN:function(e,t,n){"use strict";Object.defineProperty(t,"__esModule",{value:!0});var r={render:function(){var e=this.$createElement,t=this._self._c||e;return t("transition",{attrs:{name:"fade"}},[t("router-view")],1)},staticRenderFns:[]};var u=n("C7Lr")(null,r,!1,function(e){n("u+Ww")},null,null);t.default=u.exports},"u+Ww":function(e,t){}});
